1b7e0b4cb5bf7cceb12af2600573213278e7d2e6
Marcos Mendes <marcosdev.me@gmail.com>
2024-09-01 06:49:34 -0300

    Implements moduleNameMapper

--- jest.config.ts
+++ jest.config.ts
@@ -9,6 +9,9 @@ const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
 };
 
 export default createJestConfig(config);
